ae19e8b868f6323daf2f87aa04d1edbb5c8a92cf
Brokoli2711 <iceseam0203@gmail.com>
2023-11-21 20:49:39 +0100

    even BETTER movement system

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D), typeof(Animator))]
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    [DoNotSerialize] public Rigidbody2D rb;
     public float speed;
 
     // Start is called before the first frame update
@@ -17,10 +18,8 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float test = Input.GetAxis("Horizontal");
-        float test2 = Input.GetAxis("Vertical");
-        test *= Time.deltaTime;
-        test2 *= Time.deltaTime;
-        transform.Translate(test, test2, transform.position.z);
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        transform.Translate(horizontal * speed * Time.deltaTime, vertical * speed * Time.deltaTime, transform.position.z);
     }
 }
