46f05cf3d044e5297948ea348cb539df977c3066
Carlos7e6 <carlosagf7@gmail.com>
2023-11-23 20:06:53 +0100

    Cosas

--- Assets/Scripts/AnimationBehaviour.cs
+++ Assets/Scripts/AnimationBehaviour.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class AnimationBehaviour : MonoBehaviour
 {
+
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
     private void Awake()
     {
-        
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
+
+    
+
+
 }
