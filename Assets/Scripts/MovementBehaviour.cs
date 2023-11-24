2c46eff224fc85bfd7ce0b96ee524ed4b0828184
xUnderGame <unaigiz1@gmail.com>
2023-11-24 16:47:10 +0100

    New Input system sucks

--- Assets/Scripts/MovementBehaviour.cs
+++ Assets/Scripts/MovementBehaviour.cs
@@ -1,21 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MovementBehaviour : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Renderer rd;
 
     public void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        rd = GetComponent<Renderer>();
     }
 
     // Moves the current character.
     public void Move(float x, float y, float speed) { transform.Translate(speed * Time.deltaTime * new Vector2(x, y).normalized); }
     
-    // Makes the character "dash forward.
-    public void Dash(float x, float y, float force = 2f) {
+    // Makes the character "dash" forward.
+    public void Dash(float x, float y, float force = 15f, bool doIframes = true) {
         rb.AddForce(new Vector2(x, y) * force, ForceMode2D.Impulse);
+        if (doIframes) StartCoroutine(DashIframes());
+    }
+
+    // Changes color of the character renderer and gives some iframes (not yet implemented)
+    public IEnumerator DashIframes(float time = 0.3f) {
+        rd.material.color = new Color(0.5f, 0.5f, 0.5f);
+        yield return new WaitForSeconds(time);
+        rd.material.color = new Color(1f, 1f, 1f);
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,18 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(PolygonCollider2D), typeof(Animator))]
 public class PlayerMovement : MovementBehaviour
 {
     public float speed;
 
+    void OnEnable() {
+        GetComponent<PlayerInput>().actions["OnMove"].performed += ctx => OnMove();
+    }
+
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Move(horizontal, vertical, speed);
+        // Move(horizontal, vertical, speed);
         if (Input.GetKeyDown(KeyCode.Space)) Dash(horizontal, vertical);
+    }
+
+    // public void MovePlayer(InputAction.CallbackContext context) {
+    //     Debug.Log($"{context.ReadValue<Vector2>().x}, {context.ReadValue<Vector2>().y}");
+    //     Move(context.ReadValue<Vector2>().x, context.ReadValue<Vector2>().y, speed);
+    // }
+
+    public void OnMove() {
+        Debug.Log("hi");
     }
 }
