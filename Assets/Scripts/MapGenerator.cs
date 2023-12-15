5e446ef611c4ef847692760b77213753bed61056
Carlos <carlosagf7@gmail.com>
2023-12-15 17:39:53 +0100

    Tiles en habitacion y generacion de pasillos mal

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,84 +1,153 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System.Collections.Generic;
 
-public class TilemapRandomSizedSquaresDrawer : MonoBehaviour
+public class TilemapConnectedSquaresDrawer : MonoBehaviour
 {
     public Tilemap tilemap;
-    public TileBase tileToDraw; // La casilla que usarás para dibujar
-    public int minSquareSize = 3; // Tamaño mínimo de los cuadrados
-    public int maxSquareSize = 8; // Tamaño máximo de los cuadrados
-    public int separation = 2; // Separación entre los cuadrados
+    public TileBase tileToDraw; // La casilla que usarÃ¡s para dibujar
+    public TileBase door;
+    public TileBase wall;
+    public int separation = 8; // SeparaciÃ³n entre los cuadrados
 
     void Start()
     {
-        DrawRandomSizedSquares();
-    }
-
-    void DrawRandomSizedSquares()
-    {
         if (tilemap == null || tileToDraw == null)
             return;
-
-        // Tamaño aleatorio para el cuadrado central
-        int centerSquareSize = Random.Range(minSquareSize, maxSquareSize + 1);
-        Vector3Int centerSquareStart = new Vector3Int(-centerSquareSize / 2, -centerSquareSize / 2, 0);
-        DrawSquare(centerSquareStart, centerSquareSize);
-
-        // Dibujar cuadrados adicionales con tamaños aleatorios
-        for (int i = 0; i < 4; i++)
-        {
-            int squareSize = Random.Range(minSquareSize, maxSquareSize + 1);
-            Vector3Int squareStart = CalculateSquareStart(centerSquareStart, centerSquareSize, squareSize, i);
-            DrawSquare(squareStart, squareSize);
-            ConnectSquares(centerSquareStart, centerSquareSize, squareStart, squareSize);
-        }
+        
+        System.Random rnd = new System.Random();
+        int squareSize = rnd.Next(10,20);
+        
+        Vector3Int centerSquareStart = new Vector3Int(-squareSize / 2, -squareSize / 2, 0);
+        DrawSquare(centerSquareStart, squareSize);
+        // DrawConnectedSquares();
     }
-
-    Vector3Int CalculateSquareStart(Vector3Int centerStart, int centerSize, int squareSize, int direction)
-    {
-        // Calcula la posición inicial de un cuadrado adicional en una dirección específica
-        int totalDistance = centerSize / 2 + squareSize / 2 + separation;
-        switch (direction)
-        {
-            case 0: // Arriba
-                return new Vector3Int(centerStart.x, centerStart.y + totalDistance, 0);
-            case 1: // Abajo
-                return new Vector3Int(centerStart.x, centerStart.y - totalDistance, 0);
-            case 2: // Derecha
-                return new Vector3Int(centerStart.x + totalDistance, centerStart.y, 0);
-            case 3: // Izquierda
-                return new Vector3Int(centerStart.x - totalDistance, centerStart.y, 0);
-            default:
-                return Vector3Int.zero;
-        }
-    }
-
     void DrawSquare(Vector3Int startPosition, int squareSize)
-    {
+    {   
+        List<Vector3Int> corridorsPosition = new List<Vector3Int>();
         for (int x = 0; x < squareSize; x++)
         {
             for (int y = 0; y < squareSize; y++)
             {
                 Vector3Int tilePosition = new Vector3Int(startPosition.x + x, startPosition.y + y, 0);
-                tilemap.SetTile(tilePosition, tileToDraw);
+                if (x == 0 || y == 0 || y == squareSize - 1 || x == squareSize - 1)
+                {
+                    if ((x == squareSize / 2 && y == 0) || (x == squareSize / 2 && y == squareSize - 1) ||
+                        (x == 0 && y == squareSize / 2) || (x == squareSize - 1 && y == squareSize / 2))
+                    {
+                        if (y == squareSize - 1)
+                        {
+                            tilemap.SetTile(tilePosition, door);
+                            corridorsPosition.Add(tilePosition);
+                        }
+                        else
+                        {
+                            System.Random rndGenerateDoor = new System.Random();
+                            if (rndGenerateDoor.Next(0, 2) == 1)
+                            {
+                                tilemap.SetTile(tilePosition, door);
+                                corridorsPosition.Add(tilePosition);
+                        
+                            } else tilemap.SetTile(tilePosition, wall);
+                        }
+                    }
+                    else tilemap.SetTile(tilePosition, wall);
+                }
+                else tilemap.SetTile(tilePosition, tileToDraw);
+                
             }
+        }
+        DrawCorridors(corridorsPosition, squareSize);
+    }
+    
+    void DrawCorridors(List<Vector3Int> corridorsPosition, int squareSize)
+    {
+        for (int i = 0; i < corridorsPosition.Count; i++)
+        {
+            if (corridorsPosition[i].x == 0)
+            {
+                DrawCorridor(corridorsPosition[i],"up");
+            }
+            else if (corridorsPosition[i].y == 0)
+            {
+                DrawCorridor(corridorsPosition[i],"down");   
+            }else if (corridorsPosition[i].x == squareSize - 1)
+            {
+                DrawCorridor(corridorsPosition[i],"right");
+            }
+            else if(corridorsPosition[i].y == squareSize -1)
+            {
+                DrawCorridor(corridorsPosition[i],"left");
+            }
+        }   
+    }
+
+    void DrawCorridor(Vector3Int corridorPosition, string direction)
+    {
+        for (int i = 0; i < separation; i++)
+        {
+            Vector3Int tilePosition = new Vector3Int();
+            switch (direction)
+            {
+                case "up":
+                    tilePosition = new Vector3Int(corridorPosition.x, corridorPosition.y + i, corridorPosition.z);
+                    break;
+                case "down":
+                    tilePosition = new Vector3Int(corridorPosition.x, corridorPosition.y - i, corridorPosition.z);
+                    break;
+                case "left":
+                    tilePosition = new Vector3Int(corridorPosition.x - i, corridorPosition.y, corridorPosition.z);
+                    break;
+                case "right":
+                    tilePosition = new Vector3Int(corridorPosition.x +i, corridorPosition.y, corridorPosition.z);
+                    break;
+            }
+            tilemap.SetTile(tilePosition, door);
+        }
+       
+    }
+    void DrawConnectedSquares(Vector3Int centerSquareStart)
+    {
+         int squareSize = 0; // TamaÃ±o de los cuadrados
+        // Distancia total desde el centro hasta el inicio del cuadrado adicional
+        int totalDistance = squareSize + separation;
+
+        // Posiciones de inicio para los cuadrados adicionales
+        Vector3Int[] additionalSquaresStart = new Vector3Int[]
+        {
+            new Vector3Int(centerSquareStart.x, centerSquareStart.y + totalDistance, 0), // Arriba
+            new Vector3Int(centerSquareStart.x, centerSquareStart.y - totalDistance, 0), // Abajo
+            new Vector3Int(centerSquareStart.x + totalDistance, centerSquareStart.y, 0), // Derecha
+            new Vector3Int(centerSquareStart.x - totalDistance, centerSquareStart.y, 0)  // Izquierda
+        };
+
+        // Dibujar cuadrados adicionales y conectarlos
+        foreach (Vector3Int squareStart in additionalSquaresStart)
+        {
+            System.Random rnd = new System.Random();
+            squareSize = rnd.Next(10,20);
+            DrawSquare(squareStart, squareSize);
+            ConnectSquares(centerSquareStart, squareStart);
         }
     }
 
-    void ConnectSquares(Vector3Int firstStart, int firstSize, Vector3Int secondStart, int secondSize)
-    {
-        // Calcula los puntos medios
-        Vector3Int midPointFirst = firstStart + new Vector3Int(firstSize / 2, firstSize / 2, 0);
-        Vector3Int midPointSecond = secondStart + new Vector3Int(secondSize / 2, secondSize / 2, 0);
 
-        // Dibuja una línea de casillas entre los puntos medios
-        Vector3Int direction = midPointSecond - midPointFirst;
+
+    void ConnectSquares(Vector3Int firstSquareStart, Vector3Int secondSquareStart)
+    {
+        int squareSize = 5;
+        // Calcula los puntos medios de los lados de los cuadrados
+        Vector3Int midPointFirstSquare = firstSquareStart + new Vector3Int(squareSize / 2, squareSize / 2, 0);
+        Vector3Int midPointSecondSquare = secondSquareStart + new Vector3Int(squareSize / 2, squareSize / 2, 0);
+
+        // Dibuja una lÃ­nea de casillas entre los puntos medios
+        Vector3Int direction = (midPointSecondSquare - midPointFirstSquare);
         int steps = Mathf.Max(Mathf.Abs(direction.x), Mathf.Abs(direction.y));
         Vector3Int step = new Vector3Int(direction.x / steps, direction.y / steps, 0);
 
         for (int i = 1; i < steps; i++) // Comienza en 1 para evitar superponer el punto medio
         {
-            Vector3Int tilePosition = midPointFirst + step * i;
+            Vector3Int tilePosition = midPointFirstSquare + step * i;
             tilemap.SetTile(tilePosition, tileToDraw);
         }
     }
