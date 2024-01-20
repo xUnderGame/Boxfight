7064b5df90327a1eb179d41b5b14c76aa33f8afe
xUnderGame <unaigiz1@gmail.com>
2024-01-20 14:33:57 +0100

    Just made some conditions better, affects nothing.

--- Assets/Scripts/Map/Room.cs
+++ Assets/Scripts/Map/Room.cs
@@ -1,52 +1,42 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 public class Room : MonoBehaviour
 {
     public int id;
     public int size;
-    public List<GameObject> enemyList = new List<GameObject>();
-    public List<Vector3Int> corridorPosition = new List<Vector3Int>();
-    public List<GameObject> roomsShared = new List<GameObject>();
+    public List<GameObject> enemyList = new();
+    public List<Vector3Int> corridorPosition = new();
+    public List<GameObject> roomsShared = new();
 
     GameObject corridorObject;
     CorridorBlock corridorScript;
-
 
     private void Awake()
     {
         corridorObject = GameObject.Find("Puente");
         corridorScript = corridorObject.GetComponent<CorridorBlock>();
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") == false) return;
+        
+        Player playerScript = collision.GetComponent<Player>();
+        TrapIsOn(playerScript);
+
+        if (roomsShared.Count == 0) return;
+        for (int i = 0; i < roomsShared.Count; i++)
         {
-            Player playerScript = collision.GetComponent<Player>();
-            TrapIsOn(playerScript);
-            if (roomsShared.Count != 0)
-            {
-                for (int i = 0; i < roomsShared.Count; i++)
-                {
-                    roomsShared[i].GetComponent<Room>().TrapIsOn(playerScript);
-                }
-            }
+            roomsShared[i].GetComponent<Room>().TrapIsOn(playerScript);
         }
     }
 
     public bool CheckIfAllEnemiesDead()
     {
-        if (enemyList.Count == 0)
-        {
-            foreach (GameObject room in roomsShared)
-            {
-                if (room.GetComponent<Room>().enemyList.Count != 0) return false;
-            }
-            return true;
-        }
-        else return false;
+        if (enemyList.Count != 0) return false;
+        return !roomsShared.Any(room => { return room.GetComponent<Room>().enemyList.Count != 0; });
     }
 
     public void TrapIsOff()
